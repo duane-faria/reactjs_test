289c398d179a9917456c5ca6be483a298903bbeb
duane-faria <duane.faria@etec.sp.gov.br>
2020-11-19 22:14:56 -0300

    ...

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+enzyme.configure({
+  adapter: new EnzymeAdapter(),
 });
+
+test('renders learn react link', () => {
+  const wrapper = enzyme.shallow(<App />);
+  expect(wrapper).toBeFalsy();
+});
+ 
